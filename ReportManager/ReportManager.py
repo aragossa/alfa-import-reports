02984577449656bcc74d2c694970d424eb78b9de
Плосковитов Илья Антонович <iploskovitov@phoenixit.ru>
2020-09-20 11:41:57 +0300

    added venv

--- ReportManager/ReportManager.py
+++ ReportManager/ReportManager.py
@@ -5,25 +5,27 @@ from ReportManager.WildberriesReport.WildberriesReport import WildberriesReport
 
 log = get_logger("ReportManager")
 
+
 class ReportManager:
     def __init__(self, args):
         self.args = args
 
     def run(self):
         if len(self.args) == 1:
-            print ('Application usage:\n main.py -<report shortname1> -<report shortname1>\n    -w for Wildberries report\n    -f for flow report')
+            print(
+                'Application usage:\n main.py -<report shortname1> -<report shortname1>\n    -w for Wildberries report\n    -f for flow report')
             sys.exit(2)
         else:
             for arg in self.args:
                 if '-w' == arg:
                     self.run_wildberries()
-                elif '-f'== arg:
+                elif '-f' == arg:
                     self.run_flow()
                 elif arg == 'main.py':
                     log.info('Starting application')
                 else:
-                    print ('main.py -<report shortname1> -<report shortname1>\n    -w for Wildberries report\n    -f for flow report')
-
+                    print(
+                        'main.py -<report shortname1> -<report shortname1>\n    -w for Wildberries report\n    -f for flow report')
 
     def run_wildberries(self):
         log.info('Running Wildberries report')
